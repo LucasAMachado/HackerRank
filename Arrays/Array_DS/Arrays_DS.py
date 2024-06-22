508ccd68a63fa9cecc80b54aeb78d7587ca78103
LucasAMachado <155488066+LucasAMachado@users.noreply.github.com>
2024-06-22 17:43:12 +0300

    Arrays-DS

---
+++ Arrays/Array_DS/Arrays_DS.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+
+def reverseArray(a):
+    l, r = 0, len(a) - 1
+    res = [0] * len(a)
+
+    while l < len(a):
+        res[l] = a[r]
+
+        l += 1
+        r -= 1
+
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
